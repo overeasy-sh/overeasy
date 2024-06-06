Corrected scaling for OwlV2 bboxes

overeasy/models/detection/owlv2.py
@@ -15,12 +15,27 @@ class OwlV2(BoundingBoxModel):
     def detect(self, image: Image.Image, classes: List[str]) -> Detections:
         predictions = self.detector(image, candidate_labels=classes)
         num_preds = len(predictions)
-        xyxy = np.zeros((num_preds, 4), dtype=np.float32)
+        xyxy = np.zeros((num_preds, 4), dtype=np.int32)
         confidence = np.zeros(num_preds, dtype=np.float32)
         class_ids = np.zeros(num_preds, dtype=np.int64)
         
+        width_ratio = 1.0
+        height_ratio = 1.0
+        width = image.width
+        height = image.height
+        if width > height:
+            height_ratio = height / width
+        elif height > width:
+            width_ratio = width / height
+                
         for i, pred in enumerate(predictions):
-            xyxy[i] = [pred['box']['xmin'], pred['box']['ymin'], pred['box']['xmax'], pred['box']['ymax']]
+            x1, y1, x2, y2 = pred['box']['xmin'], pred['box']['ymin'], pred['box']['xmax'], pred['box']['ymax']
+            x1 = round(x1 / width_ratio)
+            y1 = round(y1 / height_ratio)
+            x2 = round(x2 / width_ratio)
+            y2 = round(y2 / height_ratio)
+            
+            xyxy[i] = [x1, y1, x2, y2]
             confidence[i] = pred['score']
             class_ids[i] = classes.index(pred['label'])
 